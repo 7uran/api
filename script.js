1e695ee3b0f1eb6c25a456f0d44c99de7ab84e75
Hamdullayev Turan <turanyh@code.local>
2024-06-07 21:32:26 +0400

    added search method

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 let tablo = document.querySelector(".dataTable");
-let tBody = document.querySelector("tbody"); 
+let tBody = document.querySelector("tbody");
 
 
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => {
-        addData(data); 
+        
+        // addData(data); 
+        searchData(data)
     });
 
 function addData(data) {
-    data.forEach(post => { 
+    data.forEach(post => {
         let row = document.createElement('tr');
-        
+
         Object.values(post).forEach((value) => {
             let cell = document.createElement('td');
-            cell.textContent = value; 
-            row.appendChild(cell); 
+            cell.textContent = value;
+            row.appendChild(cell);
         });
 
-        tBody.appendChild(row); 
+        tBody.appendChild(row);
+    });
+}
+
+
+function searchData(data) {
+    let value = prompt()
+    filteredData = data.filter((post) => {
+        return post.userId == value;
+
+    })
+    filteredData.forEach(post => {
+        let row = document.createElement('tr');
+
+        Object.values(post).forEach((value) => {
+            let cell = document.createElement('td');
+            cell.textContent = value;
+            row.appendChild(cell);
+        });
+
+        tBody.appendChild(row);
     });
 }
